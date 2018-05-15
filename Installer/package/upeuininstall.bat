ebe8ed7645ddde38d7050a603168d4b5306aa6ad
Ben Lubar <ben.lubar@gmail.com>
2018-05-15 13:37:57 -0500

    Fix uninstaller script.

--- Installer/package/upeuininstall.bat
+++ Installer/package/upeuininstall.bat
@@ -1,1 +1,4 @@
-romp uninstall UniversalPackageExplorer
+reg delete HKCU\Software\Inedo\UniversalPackageExplorer /f
+reg delete HKCU\Software\Classes\Applications\UniversalPackageExplorer.exe /f
+reg delete HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\UniversalPackageExplorer /f
+rd /s /q %~dp0
